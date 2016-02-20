5b8a6395de20f18726b66814db63d3b3607700f0
Gigi Wolff <gigi.wolff@gmail.com>
2016-02-20 12:49:08 -0500

    changed read accessor

--- phone_number.rb
+++ phone_number.rb
@@ -1,34 +1,37 @@
+require "pry"
+
 class PhoneNumber
   BAD_NUMBER = "0000000000".freeze
-
+  
   def initialize(num)
-    @number = num
+    @phone_number = num
   end
 
   def number
-    cleanup_number
-    valid_number? ? @number : BAD_NUMBER
+    valid_number? ? phone_number : BAD_NUMBER
   end
 
   def area_code
-    @number.slice(0..2)
+    phone_number.slice(0..2)
   end
 
   def to_s
-    number
-    "(" + area_code + ") " + @number.slice(3..5) + "-" + @number.slice(6..9)
+    "(" + area_code + ") " + phone_number.slice(3..5) + "-" + phone_number.slice(6..9)
   end
 
   private
 
   def valid_number?
-    return false if @number.length < 10 || @number.length >= 11
+    return false if phone_number.length < 10 || phone_number.length >= 11
     true
   end
 
-  def cleanup_number
-    return BAD_NUMBER unless /[[:alpha:]]/.match(@number).nil?
-    @number = @number.each_char.select { |c| /[0123456789]/.match(c) }.join
-    @number = @number.slice!(1..10) if @number.length == 11 && @number.start_with?("1")
+  def phone_number #read accessor
+    return BAD_NUMBER unless /[[:alpha:]]/.match(@phone_number).nil?
+    clean_number = @phone_number.each_char.select { |c| /[0123456789]/.match(c) }.join
+    clean_number.slice!(0) if clean_number.length == 11 && clean_number.start_with?("1")
+    @phone_number = clean_number
   end
+
 end
+
