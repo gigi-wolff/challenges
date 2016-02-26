851da535adc12e47733eecaafaff0b96a707f811
Gigi Wolff <gigi.wolff@gmail.com>
2016-02-26 15:10:24 -0500

    made change_time method private

--- clock.rb
+++ clock.rb
@@ -14,19 +14,6 @@ class Clock
     Clock.new(hr_, min_)
   end
 
-  def change_time(delta_minutes)
-    total_mins = (hr * 60) + min + delta_minutes
-    hours_and_mins = total_mins.divmod(60)
-    @min = hours_and_mins[1]
-    if hours_and_mins[0] <= 0 
-      @hr = 24 + hours_and_mins[0] 
-    elsif hours_and_mins[0] >= 24
-      @hr = hours_and_mins[0].modulo(24)
-    else
-      @hr = hours_and_mins[0]
-    end
-  end
-
   def +(add_mins)
     change_time(add_mins)
     self
@@ -45,5 +32,20 @@ class Clock
     minutes = min < 10 ? "0" + min.to_s : min.to_s
     hours = hr < 10 ? "0" + hr.to_s : hr.to_s
     hours + ":" + minutes
+  end
+
+  private
+
+  def change_time(delta_minutes)
+    total_mins = (hr * 60) + min + delta_minutes
+    hours_and_mins = total_mins.divmod(60)
+    @min = hours_and_mins[1]
+    if hours_and_mins[0] <= 0
+      @hr = 24 + hours_and_mins[0]
+    elsif hours_and_mins[0] >= 24
+      @hr = hours_and_mins[0].modulo(24)
+    else
+      @hr = hours_and_mins[0]
+    end
   end
 end
